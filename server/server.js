88b1d6b8379bb7bd016b790aed4dd73492e40620
madhavr16 <raizadamadhav16@gmail.com>
2025-06-27 17:01:37 +0530

    ..

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-require('dotenv').config()
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'development' ? '.env.local' : '.env'
+});
+
 
 const app = require('./app')
 const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(process.env.MONGO_URI_LOCAL || process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
